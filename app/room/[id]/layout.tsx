6ad4032a9d9b958cdab3abca686544ab1af07e07
tareq <115352423+x287x@users.noreply.github.com>
2025-05-03 05:50:42 +0300

    Use the viewport export in layout server components

---
+++ app/room/[id]/layout.tsx
@@ -0,0 +1,10 @@
+import { Viewport } from "next";
+
+export const viewport: Viewport = {
+  interactiveWidget: "resizes-content",
+};
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
+export default Layout;
