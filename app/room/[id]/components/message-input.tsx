e24cda9ec60383a56c97676bc5e578d635472b91
tareq <115352423+x287x@users.noreply.github.com>
2025-05-03 15:53:32 +0300

    Use textarea in room

--- app/room/[id]/components/message-input.tsx
+++ app/room/[id]/components/message-input.tsx
@@ -1,14 +1,15 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import socket from "@/lib/socket";
 import { Send } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 const MessageInput = ({ roomId }: { roomId: string }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
   const [text, setText] = useState<string>("");
 
-  const handleSubmit = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     inputRef.current?.focus();
     const value = inputRef.current?.value;
     if (value?.trim() === "" || !value) {
@@ -17,39 +18,26 @@ const MessageInput = ({ roomId }: { roomId: string }) => {
     socket.emit("punishment-message", roomId, inputRef.current?.value);
     setText("");
   };
-
-  useEffect(() => {
-    const input = inputRef.current;
-    const onKeyDown = (e: KeyboardEvent) => {
-      if (e.key === "Enter") {
-        handleSubmit();
-      }
-    };
-    input?.addEventListener("keydown", onKeyDown);
-    return () => {
-      input?.removeEventListener("keydown", onKeyDown);
-    };
-  });
-
   return (
-    <div className="flex gap-2 px-2">
-      <Input
+    <form className="flex items-center gap-2 px-2" onSubmit={handleSubmit}>
+      <Textarea
         autoFocus
         ref={inputRef}
-        type="text"
         value={text}
         placeholder="Hurry up!!!"
         onChange={(e) => {
           setText(e.target.value);
         }}
+        className="max-h-24 min-h-10 resize-none"
       />
-      <Button type="button" onClick={handleSubmit}>
+
+      <Button type="submit" className="h-10 self-end">
         <span className="hidden md:block">Send</span>
         <span className="block">
           <Send />
         </span>
       </Button>
-    </div>
+    </form>
   );
 };
 export default MessageInput;
